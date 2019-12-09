f5db0f510b33eed3661f8a3e5050f996390b7527
anaspoiala <ana.spoiala25@gmail.com>
2019-12-09 19:01:28 +0200

    day 05 part 2 done. refactored the intcode parser.

--- day 05/app.js
+++ day 05/app.js
@@ -50,8 +50,265 @@
 //
 //
 
-const inputProgramText = document.getElementById("inputProgram");
+
+// =============================== Instructions ===============================
+
+class Instruction {
+	constructor(instruction, pointerIncrementValue) {
+		this.instruction = instruction;
+		this.pointerIncrementValue = pointerIncrementValue;
+	}
+
+	static getOptcode(instruction) {
+		return instruction % 100;
+	}
+
+	nextPointerPosition(currentPointer) {
+		return currentPointer + this.pointerIncrementValue;
+	}
+
+	getParameterMode(instruction, paramNumber) {
+		let parameterModes = Math.floor(instruction / 100);
+		return Math.floor(parameterModes / Math.pow(10, paramNumber - 1)) % 10;
+	}
+
+	readNextInput(inputArray) {
+		return inputArray.shift();
+	}
+
+	getParameterValue(program, paramNumber, pointer) {
+		// If parameter is in position mode
+		return this.getParameterMode(this.instruction, paramNumber) == 0
+			? // Return value at specified position.
+			  program[program[pointer + paramNumber]]
+			: // Else return value.
+			  program[pointer + paramNumber];
+	}
+
+	execute(program, pointer, inputs, outputs) {}
+}
+
+class HaltInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 1);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		return;
+	}
+}
+
+class AddInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 4);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		let param2 = this.getParameterValue(program, 2, pointer);
+		let outputPosition = program[pointer + 3];
+
+		program[outputPosition] = param1 + param2;
+	}
+}
+
+class MultiplyInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 4);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		let param2 = this.getParameterValue(program, 2, pointer);
+		let outputPosition = program[pointer + 3];
+
+		program[outputPosition] = param1 * param2;
+	}
+}
+
+class ReadInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 2);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let outputPosition = program[pointer + 1];
+		let readInput = this.readNextInput(inputs);
+
+		program[outputPosition] = readInput;
+	}
+}
+
+class WriteInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 2);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		//let outputPosition = program[pointer + 1];
+		//let outputValue = program[outputPosition];
+		let outputValue = this.getParameterValue(program, 1, pointer);
+		outputs.push(outputValue);
+	}
+}
+
+class JumpIfTrueInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 3);
+		this.pointerPosition = 0;
+	}
+
+	nextPointerPosition(currentPointer) {
+		return this.pointerPosition;
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		if (param1 !== 0) {
+			this.pointerPosition = this.getParameterValue(program, 2, pointer);
+		} else {
+			this.pointerPosition = pointer + 3;
+		}
+	}
+}
+
+class JumpIfFalseInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 3);
+		this.pointerPosition = 0;
+	}
+
+	nextPointerPosition(currentPointer) {
+		return this.pointerPosition;
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		if (param1 === 0) {
+			this.pointerPosition = this.getParameterValue(program, 2, pointer);
+		} else {
+			this.pointerPosition = pointer + 3;
+		}
+	}
+}
+
+class LessThanInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 4);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		let param2 = this.getParameterValue(program, 2, pointer);
+		let outputPosition = program[pointer + 3];
+
+		program[outputPosition] = param1 < param2 ? 1 : 0;
+	}
+}
+
+class EqualsInstruction extends Instruction {
+	constructor(instruction) {
+		super(instruction, 4);
+	}
+
+	execute(program, pointer, inputs, outputs) {
+		let param1 = this.getParameterValue(program, 1, pointer);
+		let param2 = this.getParameterValue(program, 2, pointer);
+		let outputPosition = program[pointer + 3];
+
+		program[outputPosition] = param1 === param2 ? 1 : 0;
+	}
+}
+
+
+// =============================== Parser ===============================
+
+class IntcodeParser {
+	constructor(program, input, output) {
+		this.program = program.slice();
+		this.input = input.slice();
+		this.output = output.slice();
+		this.pointer = 0;
+	}
+
+	parseProgram() {
+		this.pointer = 0;
+
+		while (this.pointer < this.program.length) {
+			let currentInstruction = this.program[this.pointer];
+			let optcode = Instruction.getOptcode(currentInstruction);
+			let instruction;
+
+			switch (optcode) {
+				case 1:
+					instruction = new AddInstruction(currentInstruction);
+					break;
+				case 2:
+					instruction = new MultiplyInstruction(currentInstruction);
+					break;
+				case 3:
+					instruction = new ReadInstruction(currentInstruction);
+					break;
+				case 4:
+					instruction = new WriteInstruction(currentInstruction);
+					break;
+				case 5:
+					instruction = new JumpIfTrueInstruction(currentInstruction);
+					break;
+				case 6:
+					instruction = new JumpIfFalseInstruction(currentInstruction);
+					break;
+				case 7:
+					instruction = new LessThanInstruction(currentInstruction);
+					break;
+				case 8:
+					instruction = new EqualsInstruction(currentInstruction);
+					break;
+				case 99:
+					instruction = new HaltInstruction(currentInstruction);
+					break;
+				default:
+					console.log(
+						`Something went wrong at position ${this.pointer} (instruction=${currentInstruction}, optcode=${optcode})`
+					);
+					return;
+			}
+
+			console.log(
+				`Parsing instruction ${currentInstruction} (${
+					instruction.constructor.name
+				}, prevPointer=${this.pointer}, nextPointer=${instruction.nextPointerPosition(
+					this.pointer
+				)})`
+			);
+
+			if (instruction instanceof HaltInstruction) {
+				return;
+			}
+
+			instruction.execute(this.program, this.pointer, this.input, this.output);
+			this.pointer = instruction.nextPointerPosition(this.pointer);
+		}
+	}
+
+	getDiagnosticCode() {
+		return this.output[this.output.length - 1];
+	}
+
+	getOutput() {
+		return this.output.slice();
+	}
+
+	getProgram() {
+		return this.program.slice();
+	}
+}
+
+
+// ==============================================================
+
 const inputsText = document.getElementById("inputs");
+const inputProgramText = document.getElementById("inputProgram");
 const part1Button = document.getElementById("part1Button");
 const part2Button = document.getElementById("part2Button");
 const resultText = document.getElementById("result");
@@ -60,111 +317,40 @@ part1Button.addEventListener("click", () => {
 	runPart1();
 });
 
-part2Button.addEventListener("click", () => {});
+part2Button.addEventListener("click", () => {
+	runPart2();
+});
 
 function runPart1() {
 	// Get input
-	let inputs = [1];
+	let input = parseInput(inputsText);
+	let output = [];
 	let program = parseInput(inputProgramText);
 
 	// Compute result
-	let outputs = [];
-	parseProgram(program, inputs, outputs);
+	let intcodeParser = new IntcodeParser(program, input, output);
+	console.log("Begin parsing program...");
+	intcodeParser.parseProgram();
 
 	// Display result
-	resultText.innerHTML = outputs;
+	resultText.innerHTML = intcodeParser.getDiagnosticCode();
 }
 
 function runPart2() {
 	// Get input
-	let inputs = [5];
+	let input = parseInput(inputsText);
+	let output = [];
 	let program = parseInput(inputProgramText);
 
 	// Compute result
-	let outputs = [];
-	parseProgram(program, inputs, outputs);
+	let intcodeParser = new IntcodeParser(program, input, output);
+	console.log("Begin parsing program...");
+	intcodeParser.parseProgram();
 
 	// Display result
-	resultText.innerHTML = outputs;
+	resultText.innerHTML = intcodeParser.getDiagnosticCode();
 }
 
 function parseInput(inputText) {
 	return inputText.value.split(",").map(Number);
-}
-
-function parseProgram(program, inputs, outputs) {
-	let param1, param2, outputPosition, readInput, outputValue;
-	for (let i = 0; i < program.length; ) {
-		let instruction = program[i];
-		let optcode = getOptcode(instruction);
-
-		switch (optcode) {
-			case 1:
-				// Add param1 and param2. Store result at position param3 (outputPosition).
-				param1 = getParameterValue(program, instruction, 1, i);
-				param2 = getParameterValue(program, instruction, 2, i);
-				outputPosition = program[i + 3]; // output param is always in position mode
-
-				program[outputPosition] = param1 + param2;
-
-				i += 4; // increment
-				break;
-			case 2:
-				// Multiply param1 and param2. Store result at position param3 (outputPosition).
-				param1 = getParameterValue(program, instruction, 1, i);
-				param2 = getParameterValue(program, instruction, 2, i);
-				outputPosition = program[i + 3]; // output param is always in position mode
-
-				program[outputPosition] = param1 * param2;
-
-				i += 4; // increment
-				break;
-			case 3:
-				// Read from input. Save input at position param1 (outputPosition).
-				outputPosition = program[i + 1];
-
-				readInput = readNextInput(inputs);
-
-				program[outputPosition] = readInput;
-
-				i += 2; // increment
-				break;
-			case 4:
-				// Write to output. Write value from position param1 (inputPosition).
-				outputPosition = program[i + 1];
-
-				outputValue = program[outputPosition];
-
-				outputs.push(outputValue);
-
-				i += 2; // increment
-				break;
-			case 99:
-				return; // program;
-			default:
-				return `Something went wrong at position ${i} (instruction=${program[i]}, optcode=${optcode})`;
-		}
-	}
-}
-
-function getParameterValue(program, instruction, paramNumber, pos) {
-	// If parameter is in position mode
-	return getParameterMode(instruction, paramNumber) == 0
-		? // Return value at specified position.
-		  program[program[pos + paramNumber]]
-		: // Else return value.
-		  program[pos + paramNumber];
-}
-
-function getOptcode(instruction) {
-	return instruction % 100;
-}
-
-function getParameterMode(instruction, paramNumber) {
-	let parameterModes = Math.floor(instruction / 100);
-	return Math.floor(parameterModes / Math.pow(10, paramNumber - 1)) % 10;
-}
-
-function readNextInput(inputArray) {
-	return inputArray.shift();
 }
