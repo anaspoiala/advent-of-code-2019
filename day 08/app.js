f16e11b53d0c96f9dd7257983e44b5ae124d42bf
anaspoiala <ana.spoiala25@gmail.com>
2019-12-12 00:38:13 +0200

    day 08 part 2 done

--- day 08/app.js
+++ day 08/app.js
@@ -18,22 +18,29 @@ function runPart1() {
 	let [width, height] = readImageSize();
 	let layers = separateLayers(width, height, imageData);
 
-	let layerWithLeastZeroes = getLayerWithLeastZeroes(layers); 
+	let layerWithLeastZeroes = getLayerWithLeastZeroes(layers);
 
 	let noOfOneMultipiedByNoOfTwoDigits =
-        countDigit(1, layerWithLeastZeroes) * countDigit(2, layerWithLeastZeroes);
-    
-    displayOutput(noOfOneMultipiedByNoOfTwoDigits);
+		countDigit(1, layerWithLeastZeroes) * countDigit(2, layerWithLeastZeroes);
+
+	displayOutput(noOfOneMultipiedByNoOfTwoDigits);
 }
 
-function runPart2() {}
+function runPart2() {
+	let imageData = readImageData();
+	let [width, height] = readImageSize();
+	let layers = separateLayers(width, height, imageData);
+	let layerSize = width * height;
 
-function getLayerWithLeastZeroes(layers) {
-    return layers.sort(
-        (layer1, layer2) => countDigit(0, layer1) - countDigit(0, layer2)
-    )[0];
+	let decodedImage = [];
+
+	for (let i = 0; i < layerSize; i++) {
+		decodedImage.push(getPixelColor(layers, i));
+	}
+
+	let matrix = arrayToMatrix(decodedImage, width);
+	displayImage(matrix);
 }
-
 
 function readImageData() {
 	return inputImageData.value.split("").map(Number);
@@ -47,16 +54,34 @@ function displayOutput(output) {
 	resultText.innerHTML = output;
 }
 
+function displayImage(matrix) {
+	let result = '<p style="font-family:monospace"><pre>';
+	for (let line of matrix) {
+		let lineHTML = line
+			.toString()
+			.split(",")
+			.join("")
+			.replace(/1/g, "#")
+			.replace(/0/g, " ");
+		result += lineHTML + "\n";
+	}
+
+	result += "</pre></p>";
+
+	resultText.innerHTML = result;
+}
+
+/**
+ * Returns an array of arrays representing the layers present in the image.
+ */
 function separateLayers(width, height, imageDataArray) {
 	let length = imageDataArray.length;
 	let imageSize = width * height;
-	let numberOfLayers = parseInt(length / imageSize);
-	let layerSize = parseInt(length / numberOfLayers);
 
 	let layers = [];
 	let layerIndex = 0;
-	for (let i = 0; i < length; i += layerSize) {
-		layers[layerIndex] = imageDataArray.slice(i, i + layerSize);
+	for (let i = 0; i < length; i += imageSize) {
+		layers[layerIndex] = imageDataArray.slice(i, i + imageSize);
 		layerIndex++;
 	}
 
@@ -67,3 +92,39 @@ function countDigit(digit, layer) {
 	return layer.filter(d => d === digit).length;
 }
 
+/**
+ * Sorts layers ascending by the number of zeroes on it and returns first element.
+ */
+function getLayerWithLeastZeroes(layers) {
+	return layers.sort((layer1, layer2) => countDigit(0, layer1) - countDigit(0, layer2))[0];
+}
+
+/**
+ * Returns first pixel with value different from 2.
+ * Returns 2 if no other value is found.
+ */
+function getPixelColor(layers, index) {
+	let layerIndex = 0;
+
+	while (layerIndex < layers.length) {
+		let pixel = layers[layerIndex][index];
+
+		if (pixel !== 2) {
+			return pixel;
+		}
+
+		layerIndex++;
+	}
+
+	return 2; // default return value
+}
+
+function arrayToMatrix(array, elemsPerLine) {
+	let result = [];
+
+	for (let i = 0; i < array.length; i += elemsPerLine) {
+		result.push(array.slice(i, i + elemsPerLine));
+	}
+
+	return result;
+}
